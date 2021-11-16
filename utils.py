e2d7a83dfa195bc6fc0234c4e633e8c5f61e6d0c
Gregory H Eales <gregoeales@aol.com>
2021-11-16 07:58:03 -0800

    Remove old function and only group by user_id

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ def generate_data(path='./data/partner_users_dataset.csv', user_pct = 0.2):
     df = pd.read_csv(path)
     df = df.fillna(0)
     
-    print(df.shape)
+    print('loaded {} rows'.format("{:,}".format(df.shape[0])))
     
     user_ids = df['user_id'].unique()
     shuffle(user_ids)
@@ -23,45 +23,22 @@ def generate_data(path='./data/partner_users_dataset.csv', user_pct = 0.2):
     
     df = df[df['user_id'].isin(user_ids[0:num_users])]
     df = df.sort_values(by=['click_timestamp'])
-    df = df.groupby(['group_id', 'user_id'])
-    df = df.apply(lambda x: x[['result','cpi','click_timestamp','term_timestamp','status_code_1','status_code_2']].to_dict('records'))
+    df = df.groupby(['user_id'])
+    df = df.apply(lambda x: x[['result','cpi','click_timestamp','term_timestamp','status_code_1','status_code_2', 'group_id']].to_dict('records'))
     
     data = df.to_dict()
     del df
     
-    for partner_user in tqdm(list(data.keys())):
-        data[partner_user] = [list(i.values()) for i in data[partner_user]]
+    for user in tqdm(list(data.keys())):
+        data[user] = [list(i.values()) for i in data[user]]
     
-    return data
-
-
-def old_generate_data(path='./data/nqs-2021-10-03.csv', user_pct = 0.2):
-    
-    df = pd.read_csv(path)
-    df = df.fillna(0)
-    
-    print(df.shape)
-    
-    user_ids = df['user_id'].unique()
-    shuffle(user_ids)
-    num_users = len(user_ids)
-    
-    # only select a percentage of users
-    num_users = int(user_pct*num_users)
-        
-    df = df[df['user_id'].isin(user_ids[0:num_users])]
-    df = df.sort_values(by=['click_timestamp'])
-    df = df.groupby(['group_id', 'user_id'])
-    df = df.apply(lambda x: x.to_dict('records'))
-    
-    print(df.shape)
-    data = df.to_dict()
-    del df
     return data
 
 
 def main():
-    pass
+    data = generate_data(user_pct = 0.01)
+    for row in data[list(data.keys())[0]]:
+        print(row)
     
 if __name__ == '__main__':
-    pass
+    main()
